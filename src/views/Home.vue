7c065da5a2e157a305b9e974576950cdeee91cd5
Rafael Dias <rafaels.dias@me.com>
2020-05-07 16:36:24 -0300

    Criação dos componentes ProdutosBuscar e ProdutosLista

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <section>
+      <h1>Compre ou Venda</h1>
+      <ProdutosBuscar />
+      <ProdutosLista />
+    </section>
+  </div>
 </template>
 
 <script>
+import ProdutosBuscar from "../components/ProdutosBuscar";
+import ProdutosLista from "../components/ProdutosLista";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
 };
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+  font-size: 2rem;
+}
+</style>
