0b1885426fee15372eaba9f97473bcff52b8f6da
Rafael Dias <rafaels.dias@me.com>
2020-05-08 08:55:43 -0300

    range de paginas

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -3,25 +3,16 @@
     <div class="produtos" v-if="produtos && produtos.length">
       <div class="produto" v-for="(produto, index) in produtos" :key="index">
         <router-link to="/">
-          <img
-            v-if="produto.fotos"
-            :src="produto.fotos[0].src"
-            :alt="produto.fotos[0].titulo"
-          />
+          <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
           <p class="preco">{{ produto.preco }}</p>
           <h2 class="titulo">{{ produto.nome }}</h2>
           <p>{{ produto.descricao }}</p>
         </router-link>
       </div>
-      <ProdutosPaginar
-        :produtosTotal="produtosTotal"
-        :produtosPorPagina="produtosPorPagina"
-      />
+      <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina" />
     </div>
     <div v-else-if="produtos && produtos.length === 0">
-      <p class="sem-resultados">
-        Busca sem resultados. Tente buscar outro termo.
-      </p>
+      <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
     </div>
   </section>
 </template>
@@ -34,38 +25,37 @@ import { serialize } from "@/helpers.js";
 export default {
   name: "ProdutosLista",
   components: {
-    ProdutosPaginar,
+    ProdutosPaginar
   },
   data() {
     return {
       produtos: null,
       produtosPorPagina: 3,
-      produtosTotal: 0,
+      produtosTotal: 0
     };
   },
   computed: {
     url() {
       const query = serialize(this.$route.query);
       return `/produto?_limit=${this.produtosPorPagina}${query}`;
-    },
+    }
   },
   methods: {
     getProdutos() {
-      this.pro;
-      api.get(this.url).then((res) => {
+      api.get(this.url).then(res => {
         this.produtosTotal = Number(res.headers["x-total-count"]);
         this.produtos = res.data;
       });
-    },
+    }
   },
   watch: {
     url() {
       this.getProdutos();
-    },
+    }
   },
   created() {
     this.getProdutos();
-  },
+  }
 };
 </script>
 
--- src/components/ProdutosPaginar.vue
+++ src/components/ProdutosPaginar.vue
@@ -1,10 +1,10 @@
 <template>
-  <ul v-if="paginasTotal">
-    <li v-for="pagina in paginasTotal" :key="pagina">
-      <router-link :to="{ query: query(pagina) }">
-        {{ pagina }}
-      </router-link>
+  <ul v-if="paginasTotal > 1">
+    <router-link :to="{ query: query(1) }">...</router-link>
+    <li v-for="pagina in paginas" :key="pagina">
+      <router-link :to="{ query: query(pagina) }">{{ pagina }}</router-link>
     </li>
+    <router-link :to="{ query: query(paginasTotal) }">...</router-link>
   </ul>
 </template>
 
@@ -13,27 +13,41 @@ export default {
   props: {
     produtosPorPagina: {
       type: Number,
-      default: 1,
+      default: 1
     },
     produtosTotal: {
       type: Number,
-      default: 1,
-    },
+      default: 1
+    }
   },
   methods: {
     query(pagina) {
       return {
         ...this.$route.query,
-        _page: pagina,
+        _page: pagina
       };
-    },
+    }
   },
   computed: {
+    paginas() {
+      const current = Number(this.$route.query._page);
+      const range = 9;
+      const offset = Math.ceil(range / 2);
+      const total = this.paginasTotal;
+      const pagesArray = [];
+
+      for (let i = 1; i <= total; i++) {
+        pagesArray.push(i);
+      }
+      pagesArray.splice(0, current - offset);
+      pagesArray.splice(range, total);
+      return pagesArray;
+    },
     paginasTotal() {
       const total = this.produtosTotal / this.produtosPorPagina;
       return total !== Infinity ? Math.ceil(total) : 0;
-    },
-  },
+    }
+  }
 };
 </script>
 
