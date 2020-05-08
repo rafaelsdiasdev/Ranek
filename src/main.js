a0c9f884581c75fcc8a0c75d37b32248f91e5a69
Rafael Dias <rafaels.dias@me.com>
2020-05-08 11:50:35 -0300

    Adicionado pagina de produto e seus estilos

--- src/main.js
+++ src/main.js
@@ -7,6 +7,18 @@ import PaginaCarregando from "@/components/PaginaCarregando.vue";
 Vue.config.productionTip = false;
 Vue.component("PaginaCarregando", PaginaCarregando);
 
+Vue.filter("numeroPreco", (valor) => {
+  valor = Number(valor);
+  if (!isNaN(valor)) {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  } else {
+    return "";
+  }
+});
+
 new Vue({
   router,
   store,
