ac1d456a09db038d5f2db40ef9297e96fd7b88a6
Rafael Dias <rafaels.dias@me.com>
2020-05-08 17:39:10 -0300

    Função de Deslogar e criada views 'UsuarioCompras, UsuarioEditar, UsuarioProdutos, UsuarioVendas'

---
+++ src/views/usuario/UsuarioCompras.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Usuário Compras</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/usuario/UsuarioEditar.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Usuário Editar</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
